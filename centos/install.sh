ca637a70103b7e46b28a7cf3e176d08f27363581
champly <champly@outlook.com>
2021-03-04 22:05:47 +0800

    update install

--- centos/install.sh
+++ centos/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 
 # nodejs
@@ -15,3 +17,30 @@ mv squashfs-root /usr/local/nvim
 
 # vim ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin:/usr/local/nvim/usr/bin:/root/go/bin
+
+# zsh
+yum install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# vim ~/.zshrc
+# ZSH_THEME="ys"
+# plugins=(
+#   git
+#   git-flow
+#   docker
+#   kubectl
+#   brew
+#   helm
+#   zsh-autosuggestions
+#   zsh-syntax-highlighting
+#   zsh-history-substring-search
+#   zsh-completions
+# )
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
--- drawin/install.sh
+++ drawin/install.sh
@@ -14,16 +14,20 @@ echo "###### install oh-my-zsh"
 rm -rf ~/.zshrc
 rm -rf ~/.zsh_history
 rm -rf ~/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
 
 echo "###### config zsh"
 rm -rf ~/.zshrc
 rm -rf ~/.zsh_history
 ln -s ~/Dropbox/config/zsh/zshrc ~/.zshrc
 ln -s ~/Dropbox/config/zsh/zsh_history ~/.zsh_history
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/plugins/zsh-history-substring-search
+# git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/plugins/zsh-history-substring-search
 source ~/.zshrc
 
 echo "###### install fzf"
