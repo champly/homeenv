d4b1d5de70088a6bb8b0ebadffcf51b9b037538e
champly <champly@outlook.com>
2021-11-01 16:24:33 +0800

    update config

--- drawin/nvim/lua/modules/ui/nvim-tree.lua
+++ drawin/nvim/lua/modules/ui/nvim-tree.lua
@@ -6,7 +6,7 @@ require("nvim-tree.events").on_nvim_tree_ready(
 
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
+-- let g:nvim_tree_special_files = { 'README.md': 1, 'Makefile': 1, 'MAKEFILE': 1 } " List of filenames that gets highlighted with NvimTreeSpecialFile
 vim.g.nvim_tree_special_files = {}
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
@@ -40,6 +40,9 @@ require("nvim-tree").setup {
 	update_cwd = true,
 	update_focused_file = {
 		enable = true,
+	},
+	filters = {
+		dotfiles = true,
 	},
 	view = {
 		width = "19%",
