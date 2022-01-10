f700d2cdbf9f9d8f2b66404df6efe58011af184d
champly <champly@outlook.com>
2022-01-10 10:00:41 +0800

    delete unuse file

--- drawin/nvim/lua/modules/ui/dashboard.lua
+++
@@ -1,7 +0,0 @@
-vim.g.dashboard_footer_icon = "🐬 "
-vim.g.dashboard_preview_command = "cat"
-vim.g.dashboard_preview_pipeline = "lolcat"
-vim.g.dashboard_preview_file = "~/.config/nvim/static/champly.txt"
-vim.g.dashboard_preview_file_height = 12
-vim.g.dashboard_preview_file_width = 58
-vim.g.dashboard_default_executive = "telescope"
