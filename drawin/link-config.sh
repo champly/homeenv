21b5996e37e096904b79e704c56022b3a67ea490
champly <champly@outlook.com>
2024-08-08 20:46:39 +0800

    update auto link script

---
+++ drawin/link-config.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+SourcePath=~/go/src/github.com/champly/homeenv/drawin
+
+Components="nvim joshuto gitui yazi starship.toml"
+
+for cmp in ${Components};
+do
+	rm -rf ~/.config/${cmp}
+	ln -s ${SourcePath}/${cmp} ~/.config/${cmp}
+done
+
+####### special
+# tmux
+rm -rf ~/.tmux.conf
+ln -s ${SourcePath}/tmux/tmux.conf ~/.tmux.conf
