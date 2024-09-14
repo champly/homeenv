8f3ba491b5e7a83ea58a1648b95f01c2cb0f43a8
champly <champly@outlook.com>
2024-09-14 10:25:30 +0800

    update nvim nvim-tree config

--- drawin/nvim/lua/modules/ui/config.lua
+++ drawin/nvim/lua/modules/ui/config.lua
@@ -302,6 +302,14 @@ function config.bufferline()
 			modified_icon = "✥",
 			buffer_close_icon = "",
 			always_show_bufferline = false,
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					highlight = "Directory",
+					separator = true -- use a "true" to enable the default, or set your own character
+				}
+			}
 		}
 	}
 
