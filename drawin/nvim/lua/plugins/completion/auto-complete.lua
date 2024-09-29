9a72c708a3ac26d4614feffa44f62bd6897f048a
champly <champly@outlook.com>
2024-09-29 20:10:26 +0800

    update nvim snippets

--- drawin/nvim/lua/plugins/completion/auto-complete.lua
+++ drawin/nvim/lua/plugins/completion/auto-complete.lua
@@ -97,7 +97,7 @@ return {
 				delete_check_events = "TextChanged",
 			})
 			require("luasnip.loaders.from_vscode").lazy_load({
-				paths = { "./external/snippet/" },
+				paths = { "./external/snippets/" },
 			})
 		end
 	},
