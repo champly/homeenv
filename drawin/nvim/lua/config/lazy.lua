b8ac00c1dda7327bb4fe420c71f3f22fa41dca3f
champly <champly@outlook.com>
2024-10-07 20:58:27 +0800

    update(nvim): lazy plugin not check update

--- drawin/nvim/lua/config/lazy.lua
+++ drawin/nvim/lua/config/lazy.lua
@@ -34,5 +34,5 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	-- checker = { enabled = true },
 })
