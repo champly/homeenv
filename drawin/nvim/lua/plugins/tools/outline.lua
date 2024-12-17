20ce057312c24c7ac1e4651acc9b08f8558912ac
champly <champly@outlook.com>
2024-12-17 17:27:08 +0800

    nvim: udpate outline config

--- drawin/nvim/lua/plugins/tools/outline.lua
+++ drawin/nvim/lua/plugins/tools/outline.lua
@@ -3,8 +3,10 @@ return {
 	-- symbols outline
 	{
 		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
 		keys = {
-			{ "<space>o", ":Outline<cr>", desc = "outline" },
+			{ "<space>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
 		},
 		config = function()
 			require("outline").setup({
