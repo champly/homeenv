00d38cffd94d06f674a8172ed881586f6b989648
champly <champly@outlook.com>
2025-02-10 11:10:01 +0800

    yazi: update version

--- drawin/yazi/plugins/git.yazi/main.lua
+++ drawin/yazi/plugins/git.yazi/main.lua
@@ -1,3 +1,5 @@
+--- @since 25.2.7
+
 local WIN = ya.target_family() == "windows"
 local PATS = {
 	{ "[MT]", 6 }, -- Modified
@@ -145,11 +147,11 @@ local function setup(st, opts)
 		end
 
 		if not change or signs[change] == "" then
-			return ui.Line("")
+			return ""
 		elseif self._file:is_hovered() then
-			return ui.Line { ui.Span(" "), ui.Span(signs[change]) }
+			return ui.Line { " ", signs[change] }
 		else
-			return ui.Line { ui.Span(" "), ui.Span(signs[change]):style(styles[change]) }
+			return ui.Line { " ", ui.Span(signs[change]):style(styles[change]) }
 		end
 	end, opts.order)
 end
@@ -159,7 +161,7 @@ local function fetch(_, job)
 	local repo = root(cwd)
 	if not repo then
 		remove(tostring(cwd))
-		return 1
+		return true
 	end
 
 	local paths = {}
@@ -175,8 +177,7 @@ local function fetch(_, job)
 		:stdout(Command.PIPED)
 		:output()
 	if not output then
-		ya.err("Cannot spawn git command, error: " .. err)
-		return 0
+		return true, Err("Cannot spawn `git` command, error: %s", err)
 	end
 
 	local changed, ignored = {}, {}
@@ -202,7 +203,7 @@ local function fetch(_, job)
 	end
 	add(tostring(cwd), repo, changed)
 
-	return 3
+	return false
 end
 
 return { setup = setup, fetch = fetch }
