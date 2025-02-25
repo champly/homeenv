9543a7a11089f575457ba7b9d42dad2eef7a45b4
champly <champly@outlook.com>
2025-02-25 10:15:12 +0800

    nvim: code action lazy load

--- drawin/nvim/lua/plugins/completion/code-action.lua
+++ drawin/nvim/lua/plugins/completion/code-action.lua
@@ -5,12 +5,8 @@ return {
 			{ "nvim-lua/plenary.nvim" },
 			{ "nvim-telescope/telescope.nvim" },
 		},
-		event = "LspAttach",
-		config = function()
-			require("tiny-code-action").setup()
-
-			vim.keymap.set("n", "<leader>ca", function() require("tiny-code-action").code_action() end,
-				{ noremap = true, silent = true })
-		end
+		keys = {
+			{ "<leader>ca", function() require("tiny-code-action").code_action() end, "Code Action" },
+		},
 	}
 }
