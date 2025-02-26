4672e49be690d45ae1ddcad06f8d6c48a52307d7
champly <champly@outlook.com>
2025-02-26 11:31:12 +0800

    nvim: fix code action keymaps

--- drawin/nvim/lua/plugins/completion/code-action.lua
+++ drawin/nvim/lua/plugins/completion/code-action.lua
@@ -5,8 +5,15 @@ return {
 			{ "nvim-lua/plenary.nvim" },
 			{ "nvim-telescope/telescope.nvim" },
 		},
-		keys = {
-			{ "<leader>ca", function() require("tiny-code-action").code_action() end, "Code Action" },
-		},
+		-- keys = {
+		--     { "<leader>ca", function() require("tiny-code-action").code_action() end, mode = { "n" }, desc = "Code Action" },
+		-- },
+		event = "LspAttach",
+		config = function()
+			require("tiny-code-action").setup()
+			vim.keymap.set("n", "<leader>ca", function()
+				require("tiny-code-action").code_action()
+			end, { noremap = true, silent = true })
+		end
 	}
 }
--- drawin/nvim/lua/plugins/tools/search.lua
+++ drawin/nvim/lua/plugins/tools/search.lua
@@ -3,15 +3,15 @@ return {
 	-- search and replace
 	{
 		"windwp/nvim-spectre",
-		keys = {
-			{ "<leader>S",  mode = { "n" }, function() require("spectre").toggle() end,                                 desc = "Toggle Spectre" },
-			{ "<leader>sw", mode = { "n" }, function() require("spectre").open_visual({ select_word = true }) end,      desc = "Search current word" },
-			{ "<leader>sw", mode = { "v" }, function() require("spectre").open_visual() end,                            desc = "Search current word" },
-			{ "<leader>sp", mode = { "n" }, function() require("spectre").open_file_search({ select_word = true }) end, desc = "Search on current file" }
-		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
+		},
+		keys = {
+			{ "<leader>S",  function() require("spectre").toggle() end,                                 mode = { "n" }, desc = "Toggle Spectre" },
+			{ "<leader>sw", function() require("spectre").open_visual({ select_word = true }) end,      mode = { "n" }, desc = "Search current word" },
+			{ "<leader>sw", function() require("spectre").open_visual() end,                            mode = { "v" }, desc = "Search current word" },
+			{ "<leader>sp", function() require("spectre").open_file_search({ select_word = true }) end, mode = { "n" }, desc = "Search on current file" }
 		},
 		opts = function()
 			local opt = {}
